bf07b46891aa76b5b772d4649b79e4de495516fa
nopalh <nopalh25@gmail.com>
2026-06-13 21:13:17 +0700

    edit query nopal

--- queries_anggota2_naufal.sql
+++ queries_anggota2_naufal.sql
@@ -1,10 +1,10 @@
 -- QUERY ANGGOTA 2 - MUHAMMAD NAUFAL HADAYA SETIAWAN (5025241181)
 
 -- 1. QUERY SEARCHING (JOIN) #1
--- Slot waktu paling ramai pada tanggal tertentu untuk membantu kantin menyiapkan kapasitas layanan
+-- Slot waktu paling ramai untuk membantu kantin menyiapkan kapasitas layanan
+select * from pre_order;
 
 SELECT
-    po.tanggal_ambil,
     k.nama_kantin,
     k.lokasi,
     sw.label AS sesi,
@@ -16,16 +16,14 @@ SELECT
 FROM pre_order po
 JOIN Sesi_Waktu sw ON po.Sesi_Waktu_id_slo = sw.id_slot
 JOIN Kantin k ON sw.Kantin_id_kantin = k.id_kantin
-WHERE po.status NOT IN ('cancelled')
-  AND po.tanggal_ambil = DATE '2026-05-28'
-GROUP BY po.tanggal_ambil, k.nama_kantin, k.lokasi, sw.label, sw.jam_mulai, sw.jam_selesai, sw.kapasitas_order
+WHERE po.status NOT IN ('cancelled') AND po.tanggal_ambil = CURRENT_DATE
+GROUP BY k.nama_kantin, k.lokasi, sw.label, sw.jam_mulai, sw.jam_selesai, sw.kapasitas_order
 ORDER BY total_order DESC;
 
 -- 2. QUERY SEARCHING (JOIN) #2
 -- Revenue per kantin per bulan (pendapatan bulanan)
 SELECT 
     k.nama_kantin,
-    k.lokasi,
     TO_CHAR(po.tanggal_ambil, 'YYYY-MM') AS bulan,
     COUNT(po.id_pre_order) AS jumlah_order,
     SUM(po.total_harga) AS total_revenue,
@@ -53,7 +51,6 @@ SELECT
     pb.kode_unik,
     pb.expired_at,
     pb.paid_at,
-    po.status AS status_order,
     po.total_harga
 FROM pembayaran pb
 JOIN pre_order po ON pb.pre_order_id_pre = po.id_pre_order
@@ -61,8 +58,11 @@ JOIN mahasiswa m ON po.mahasiswa_id_us = m.id_user
 JOIN Payment_gateway pg ON pb.Payment_gateway = pg.id_metode
 ORDER BY pb.expired_at DESC;
 
+select * from v_status_pembayaran;
+
 -- 4. VIEW #2
 -- v_kantin_rating: Kantin dengan rating, jumlah order, dan pemilik
+DROP VIEW IF EXISTS v_kantin_rating;
 
 CREATE OR REPLACE VIEW v_kantin_rating AS
 SELECT 
@@ -71,18 +71,29 @@ SELECT
     k.lokasi,
     k.rating,
     pk.nama AS nama_pemilik,
-    pk.no_hp AS kontak_pemilik,
     k.jam_buka,
     k.jam_tutup,
-    COUNT(DISTINCT po.id_pre_order) AS total_order,
-    COALESCE(SUM(po.total_harga), 0) AS total_pendapatan
+    COUNT(DISTINCT po.id_pre_order) AS total_order
 FROM Kantin k
-JOIN Pemilik_Kantin pk ON k.Pemilik_Kantin_id_p = pk.id_pemilik
-LEFT JOIN Sesi_Waktu sw ON k.id_kantin = sw.Kantin_id_kantin
-LEFT JOIN pre_order po ON sw.id_slot = po.Sesi_Waktu_id_slo AND po.status = 'completed'
-GROUP BY k.id_kantin, k.nama_kantin, k.lokasi, k.rating, 
-         pk.nama, pk.no_hp, k.jam_buka, k.jam_tutup
-ORDER BY k.rating DESC;
+LEFT JOIN Pemilik_Kantin pk 
+    ON k.Pemilik_Kantin_id_p = pk.id_pemilik
+LEFT JOIN Sesi_Waktu sw 
+    ON k.id_kantin = sw.Kantin_id_kantin
+LEFT JOIN pre_order po 
+    ON sw.id_slot = po.Sesi_Waktu_id_slo 
+    AND po.status = 'completed'
+    AND po.status_bayar = 'paid'
+GROUP BY 
+    k.id_kantin, 
+    k.nama_kantin, 
+    k.lokasi, 
+    k.rating, 
+    pk.nama, 
+    pk.no_hp, 
+    k.jam_buka, 
+    k.jam_tutup;
+
+select * from v_kantin_rating;
 
 -- 5. TRIGGER #1
 -- trg_auto_expired_bayar: Set expired_at otomatis 24 jam setelah pembayaran dibuat
@@ -91,7 +102,7 @@ CREATE OR REPLACE FUNCTION fn_trg_auto_expired_bayar()
 RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.expired_at IS NULL THEN
-        NEW.expired_at := CURRENT_TIMESTAMP + INTERVAL '24 hours';
+        NEW.expired_at := CURRENT_TIMESTAMP + INTERVAL '1 hours';
     END IF;
     RETURN NEW;
 END;
@@ -101,6 +112,34 @@ CREATE OR REPLACE TRIGGER trg_auto_expired_bayar
 BEFORE INSERT ON pembayaran
 FOR EACH ROW
 EXECUTE FUNCTION fn_trg_auto_expired_bayar();
+
+INSERT INTO pembayaran (
+    id_pembayaran,
+    pre_order_id_pre,
+    Payment_gateway,
+    metode,
+    status,
+    jumlah,
+    kode_unik
+)
+VALUES (
+    'PBY-TEST-001',
+    'PRE001',
+    'PAY001',
+    'QRIS',
+    'pending',
+    25000,
+    'UNQ-001'
+);
+
+SELECT 
+    id_pembayaran,
+    expired_at,
+    CURRENT_TIMESTAMP AS waktu_sekarang,
+    expired_at - CURRENT_TIMESTAMP AS selisih
+FROM pembayaran
+WHERE id_pembayaran = 'PBY-TEST-001';
+
 
 -- 6. TRIGGER #2
 -- trg_log_status_change: Log setiap perubahan status order ke tabel log
@@ -121,42 +160,70 @@ AFTER UPDATE OF status ON pre_order
 FOR EACH ROW
 EXECUTE FUNCTION fn_trg_log_status_change();
 
+--test
+select * from log_status_order;
+select * from pre_order;
+
+update pre_order
+set status =  'completed'
+where id_pre_order = 'PRE003';
+
 -- 7. FUNCTION #1
 -- fn_cek_kapasitas_slot: Cek sisa kapasitas slot pada tanggal tertentu
 
-CREATE OR REPLACE FUNCTION fn_cek_kapasitas_slot(
-    p_id_slot VARCHAR,
-    p_tanggal DATE
+DROP FUNCTION IF EXISTS fn_cek_kapasitas_slot(VARCHAR, DATE);
+DROP FUNCTION IF EXISTS fn_cek_kapasitas_slot(VARCHAR);
+
+CREATE OR REPLACE FUNCTION fn_cek_kapasitas_kantin(
+    p_id_kantin VARCHAR
 )
 RETURNS TABLE(
-    id_slot VARCHAR,
-    label TEXT,
+    tanggal_ambil DATE,
+    id_kantin VARCHAR,
     nama_kantin TEXT,
-    kapasitas_total INT,
-    terisi INT,
+    id_slot VARCHAR,
+    sesi TEXT,
+    jam_mulai TIME,
+    jam_selesai TIME,
+    kapasitas_order INT,
+    total_order INT,
     sisa_kapasitas INT,
-    is_tersedia BOOLEAN
+    status_kapasitas TEXT
 ) AS $$
 BEGIN
     RETURN QUERY
     SELECT 
+        po.tanggal_ambil,
+        k.id_kantin,
+        k.nama_kantin,
         sw.id_slot,
         sw.label,
-        k.nama_kantin,
+        sw.jam_mulai,
+        sw.jam_selesai,
         sw.kapasitas_order,
-        COALESCE(COUNT(po.id_pre_order)::INT, 0),
-        (sw.kapasitas_order - COALESCE(COUNT(po.id_pre_order)::INT, 0)),
-        (sw.kapasitas_order - COALESCE(COUNT(po.id_pre_order)::INT, 0)) > 0
-    FROM Sesi_Waktu sw
-    JOIN Kantin k ON sw.Kantin_id_kantin = k.id_kantin
-    LEFT JOIN pre_order po ON sw.id_slot = po.Sesi_Waktu_id_slo 
-        AND po.tanggal_ambil = p_tanggal
+        COUNT(po.id_pre_order)::INT AS total_order,
+        (sw.kapasitas_order - COUNT(po.id_pre_order)::INT) AS sisa_kapasitas,
+        CASE
+            WHEN COUNT(po.id_pre_order) >= sw.kapasitas_order THEN 'Penuh'
+            WHEN COUNT(po.id_pre_order) >= sw.kapasitas_order * 0.8 THEN 'Hampir penuh'
+            ELSE 'Tersedia'
+        END AS status_kapasitas
+    FROM Kantin k
+    JOIN Sesi_Waktu sw ON k.id_kantin = sw.Kantin_id_kantin
+    JOIN pre_order po ON sw.id_slot = po.Sesi_Waktu_id_slo
         AND po.status NOT IN ('cancelled')
-    WHERE sw.id_slot = p_id_slot
+    WHERE k.id_kantin = p_id_kantin
       AND sw.is_active = TRUE
-    GROUP BY sw.id_slot, sw.label, k.nama_kantin, sw.kapasitas_order;
+    GROUP BY po.tanggal_ambil, k.id_kantin, k.nama_kantin, sw.id_slot,
+             sw.label, sw.jam_mulai, sw.jam_selesai, sw.kapasitas_order
+    ORDER BY po.tanggal_ambil DESC, sw.jam_mulai;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- test
+SELECT * FROM fn_cek_kapasitas_kantin('KNT001');
+
 
 -- Contoh pemanggilan:
 -- SELECT * FROM fn_cek_kapasitas_slot('SLT001', '2026-05-15');
@@ -214,6 +281,50 @@ BEGIN
     END IF;
 END;
 $$;
+
+
+SELECT 
+    pb.id_pembayaran,
+    pb.status AS status_pembayaran,
+    pb.paid_at,
+    po.id_pre_order,
+    po.kode_order,
+    po.status AS status_order,
+    po.status_bayar
+FROM pembayaran pb
+JOIN pre_order po ON pb.pre_order_id_pre = po.id_pre_order
+WHERE pb.status = 'pending'
+LIMIT 5;
+
+CALL sp_proses_pembayaran('PBY-PRE001-180', 'success');
+CALL sp_proses_pembayaran('PBY-PRE004-844', 'success');
+
+SELECT 
+    pb.id_pembayaran,
+    pb.status AS status_pembayaran,
+    pb.paid_at,
+    po.id_pre_order,
+    po.kode_order,
+    po.status AS status_order,
+    po.status_bayar,
+    po.updated_at
+FROM pembayaran pb
+JOIN pre_order po ON pb.pre_order_id_pre = po.id_pre_order
+WHERE pb.id_pembayaran = 'PBY-PRE004-844';
+
+
+SELECT 
+    pb.id_pembayaran,
+    pb.status AS status_pembayaran,
+    pb.paid_at,
+    po.id_pre_order,
+    po.kode_order,
+    po.status AS status_order,
+    po.status_bayar,
+    po.updated_at
+FROM pembayaran pb
+JOIN pre_order po ON pb.pre_order_id_pre = po.id_pre_order
+WHERE pb.id_pembayaran = 'PBY-PRE001-180';
 
 -- Contoh pemanggilan:
 -- CALL sp_proses_pembayaran('PBY-PRE001-123', 'success');
